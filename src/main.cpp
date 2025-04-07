fad2d081cadf1e8ab1b87e12931f49b94bb3c228
Fredrik Storm <fredrik@fldc.se>
2025-04-07 11:59:45 +0200

    Basic simulation structure Simulation will need separate task, the firmware will not run target board properly.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  // Initialize serial communication at 115200 baud
+  Serial.begin(115200);
+  // Add your setup code here
+}
+
+void loop() {
+  // Add your main code here, to run repeatedly
+  Serial.println("Hello, ESP32!");
+  delay(1000); // Wait for 1 second
+}
