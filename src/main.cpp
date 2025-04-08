03aa91ac69beb82721ba03c2746d73d07a2643a1
Eric Grahn <eric.grahn@chasacademy.se>
2025-04-08 15:00:30 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,6 @@ void setup() {
 
 void loop() {
   // Add your main code here, to run repeatedly
-  Serial.println("Hello, ESP32!");
+  Serial.println("Hello from issue #37 (Bla bla bla)");
   delay(1000); // Wait for 1 second
 }
