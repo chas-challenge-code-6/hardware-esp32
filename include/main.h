09d51b04d6e9fab55bc6e8d990eb5b8292421131
Filip Andersson <andersson.filip98@gmail.com>
2025-05-07 15:32:42 +0200

    Base version of battery status shit, not done, some errors

--- include/main.h
+++ include/main.h
@@ -13,4 +13,9 @@ uint8_t const DHT_PIN{20};
 #define MQ2
 uint8_t const MQ2_PIN{34};
 
+#define BATTERY
+uint8_t const BOARD_BAT_ADC_PIN{35}; // ADC pin for battery voltage measurement
+uint8_t const BOARD_POWERON_PIN{23}; // Power on pin for the board
+uint8_t const BOARD_BATTERY_VOLTAGE_DIVIDER{2}; // Voltage divider for battery measurement
+
 #endif
