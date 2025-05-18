ab3a47471da3085d5ae2dc7c99806311b4e02391
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-18 23:54:48 +0200

    Fall detection logic

--- src/tasks/accelerometerTask.cpp
+++ src/tasks/accelerometerTask.cpp
@@ -7,14 +7,13 @@ extern QueueHandle_t dataQueue;
 
 void accelTask(void *pvParameters)
 {
-
     sensor_data_t accelData = {};
     SensorAccelerometer accel;
     bool initialized = false;
 
     for (size_t i = 0; i < 3; i++)
     {
-        if(accel.begin())
+        if (accel.begin())
         {
             initialized = true;
             break;
@@ -22,23 +21,31 @@ void accelTask(void *pvParameters)
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 
-    if(!initialized)
+    if (!initialized)
     {
         Serial.println("[Accelerometer Task] Failed to initialize accelerometer, deleting task.");
         vTaskDelete(NULL);
     }
-    
+
     while (true)
     {
         accel.update();
 
         accelData.accelTotal = accel.getTotal();
         accelData.accelPitch = accel.getPitch();
-        accelData.accelRoll = accel.getPitch();
+        accelData.accelRoll = accel.getRoll();
 
         Serial.printf("Acc: %.2f g | Pitch: %.2f° | Roll: %.2f°\n", accelData.accelTotal,
                       accelData.accelPitch, accelData.accelRoll);
 
+        // Fall detection
+        if (accelData.accelTotal > ACC_THRESHOLD && (abs(accelData.accelPitch) > ANGLE_THRESHOLD ||
+                                                     abs(accelData.accelRoll) > ANGLE_THRESHOLD))
+        {
+            Serial.println("[Accelerometer Task] FALL DETECTED!");
+        }
+
+        // Pedometer
         // if (xQueueSend(dataQueue, &accelData, portMAX_DELAY) != pdPASS)
         //     Serial.println("[Accelerometer Task] Data sent to queues successfully.");
         // {
