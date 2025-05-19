ff0d95cd37d8dec5a892302c52e61c90a4098daa
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-20 00:32:00 +0200

    formatting

--- include/battery.h
+++ include/battery.h
@@ -1,36 +1,35 @@
 #ifndef BATTERY_H
 #define BATTERY_H
+
 #include "utilities.h"
 #include <Arduino.h>
-#include <esp32-hal-adc.h> /// Include the ESP32 ADC library for analogReadMilliVolts
+#include <esp32-hal-adc.h>
 
-class Battery 
+class Battery
 {
-    private:
-        int batteryPin;
-        float ADCbatteryVoltage;
-        float rawBatteryVoltage;
-        float batteryVoltageLimit;
-        float batteryPercentage;
-        int voltageDivider;
-        
+private:
+    int batteryPin;
+    float ADCbatteryVoltage;
+    float rawBatteryVoltage;
+    float batteryVoltageLimit;
+    float batteryPercentage;
+    int voltageDivider;
 
-    public:
-        //Functions for monitoring battery status
-        void begin();
-        void loop();
-        float getBatteryStatus();
-        float getBatteryVoltage();
-        void getUpdate();
-        void sendData(); //For sending battery status to the server
+public:
+    // Functions for monitoring battery status
+    void begin();
+    void loop();
+    float getBatteryStatus();
+    float getBatteryVoltage();
+    void getUpdate();
+    void sendData(); // For sending battery status to the server
 
-        //Functions for power managment
-        void powerSaveMode();
-        bool isPowerOn(int pin);
-        void turnOnPower(int pin);
-        void turnOffPower(int pin);
-        void safetyShutdown(float pin, float VoltLimit);
-        
+    // Functions for power managment
+    void powerSaveMode();
+    bool isPowerOn(int pin);
+    void turnOnPower(int pin);
+    void turnOffPower(int pin);
+    void safetyShutdown(float pin, float VoltLimit);
 };
 
-#endif //BATTERY_H
+#endif
