f5f096e205ababfec35c865cfe1805017bf4ad66
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-20 00:35:41 +0200

    Add network check to gastask, only send if data was changed.

--- src/tasks/gasTask.cpp
+++ src/tasks/gasTask.cpp
@@ -4,25 +4,47 @@
 #include <Arduino.h>
 
 extern QueueHandle_t dataQueue;
+extern EventGroupHandle_t networkEventGroup;
+#define NETWORK_CONNECTED_BIT BIT0
+
+void sendGasData(sensor_data_t &gasData)
+{
+    EventBits_t bits = xEventGroupGetBits(networkEventGroup);
+
+    if (bits & NETWORK_CONNECTED_BIT)
+    {
+        if (xQueueSend(dataQueue, &gasData, portMAX_DELAY) != pdPASS)
+        {
+            Serial.println("[Gas Task] Failed to send data to queue");
+        }
+        else
+        {
+            Serial.println("[Gas Task] Data sent to queue successfully.");
+        }
+    }
+}
 
 void gasTask(void *parameter)
 {
     sensor_data_t gasData = {};
     MQ2Sensor gasSensor(GAS_PIN);
+    float oldGasPPM = NAN;
+    float newGasLevel = NAN;
 
     while (true)
     {
         gasSensor.update();
+        newGasLevel = gasSensor.getValue();
 
-        gasData.gasLevel = gasSensor.getValue();
+        Serial.printf("[Gas Task] Gas Concentration (PPM): %.2f\n", newGasLevel);
 
-        Serial.printf("[Gas Task] Gas Concentration (PPM): %.2f\n", gasData.gasLevel);
-
-        if(xQueueSend(dataQueue, &gasData, portMAX_DELAY) != pdPASS)
+        // TODO: Set threshold for sending data instead, the span could be bigger
+        if (isnan(oldGasPPM) || newGasLevel != oldGasPPM)
         {
-            Serial.println("[Gas Task] Failed to send data to queue");
+            gasData.gasLevel = newGasLevel;
+            sendGasData(gasData);
+            oldGasPPM = newGasLevel;
         }
-
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
