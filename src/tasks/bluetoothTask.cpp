4bd2e7eb7020f568ec5b149e4d57880041c2ea6e
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-20 01:46:50 +0200

    BTTask: check network and send only on change.

--- src/tasks/bluetoothTask.cpp
+++ src/tasks/bluetoothTask.cpp
@@ -5,24 +5,43 @@
 #include <Arduino.h>
 
 extern QueueHandle_t dataQueue;
+extern EventGroupHandle_t networkEventGroup;
+#define NETWORK_CONNECTED_BIT BIT0
+
+void sendBluetoothData(sensor_data_t &heartRateData)
+{
+    EventBits_t bits = xEventGroupGetBits(networkEventGroup);
+    if (bits & NETWORK_CONNECTED_BIT)
+    {
+        if (xQueueSend(dataQueue, &heartRateData, portMAX_DELAY) != pdPASS)
+        {
+            Serial.println("[Bluetooth Task] Failed to send heart rate data to queue");
+        }
+        else
+        {
+            Serial.println("[Bluetooth Task] Data sent to queue successfully.");
+        }
+    }
+}
 
 void bluetoothTask(void *pvParameters)
 {
     sensor_data_t heartRateData = {};
     BluetoothClient bClient;
+    float oldHeartRate = NAN;
+    float newHeartRate = NAN;
 
     while (true)
     {
-        heartRateData.heartRate = bClient.getHeartRate();
-
-        if (xQueueSend(dataQueue, &heartRateData, pdMS_TO_TICKS(100)) != pdPASS) {
-            Serial.println("[Bluetooth Task] Failed to send heart rate data to queue");
+        newHeartRate = bClient.getHeartRate();
+        if (isnan(oldHeartRate) || newHeartRate != oldHeartRate)
+        {
+            heartRateData.heartRate = newHeartRate;
+            sendBluetoothData(heartRateData);
+            oldHeartRate = newHeartRate;
         }
-        else {
-            Serial.print("[Bluetooth Task] Heart Rate: ");
-            Serial.println(heartRateData.heartRate);
-        }
-
+        Serial.print("[Bluetooth Task] Heart Rate: ");
+        Serial.println(newHeartRate);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
