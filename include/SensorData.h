4be3e2f37989a577e4917c41857ea64643c8b398
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-20 00:32:43 +0200

    Initialize struct data.

--- include/SensorData.h
+++ include/SensorData.h
@@ -5,17 +5,18 @@
 
 typedef struct SensorData
 {
-    uint8_t device_battery;
-    float accelZ;
-    float accelTotal;
-    float accelPitch;
-    float accelRoll;
-    bool fall_detected;
-    float temperature;
-    float humidity;
-    float gasLevel;
-    uint32_t steps;
-    uint16_t heartRate;
+    uint8_t device_battery{0};
+    uint8_t noise_level{0};
+    float accelZ{0.0};
+    float accelTotal{0.0};
+    float accelPitch{0.0};
+    float accelRoll{0.0};
+    bool fall_detected{false};
+    float temperature{0.0};
+    float humidity{0.0};
+    float gasLevel{0.0};
+    uint32_t steps{0};
+    uint16_t heartRate{0};
 } sensor_data_t;
 
 typedef struct
