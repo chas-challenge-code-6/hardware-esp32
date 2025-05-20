43c02647ef58100d31e81e633511bbf30cc5e5d1
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-20 08:21:17 +0200

    Add missing data to json.

--- src/tasks/processingTask.cpp
+++ src/tasks/processingTask.cpp
@@ -1,5 +1,6 @@
 #include "tasks/processingTask.h"
 #include "SensorData.h"
+#include "main.h"
 #include <Arduino.h>
 
 extern QueueHandle_t dataQueue;
@@ -8,10 +9,11 @@ extern QueueHandle_t httpQueue;
 bool createJson(const sensor_data_t &data, char *buffer, size_t bufferSize)
 {
     int len = snprintf(buffer, bufferSize,
-                       "{\"steps\": %d, \"humidity\": %d, \"gas\": %d, \"fall_detected\": %d, "
-                       "\"device_battery\": %d, \"heart_rate\": %d, \"noise_level\": %d }",
-                       data.steps, data.temperature, data.humidity, data.fall_detected,
-                       data.device_battery);
+                       "{\"device_id\": \"%s\" \"sensors\": { \"steps\": %d, \"humidity\": %.2f, "
+                       "\"gas\": \"ppm\": %d, \"fall_detected\": %d, "
+                       "\"device_battery\": %d, \"heart_rate\": %d, \"noise_level\": %d } }",
+                       DEVICE_ID, data.steps, data.humidity, data.gasLevel, data.fall_detected,
+                       data.device_battery, data.heartRate, data.noise_level);
     if (len < 0 || len >= bufferSize)
     {
         Serial.println("[Processing Task] JSON creation failed or truncated.");
