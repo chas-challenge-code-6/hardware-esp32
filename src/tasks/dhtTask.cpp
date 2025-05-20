64baa7118a84039ede99c1f4034041f221ffd89c
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-20 08:30:33 +0200

    DHTtask: only send data on network connection Also add threshold to avoid sending new data too frequently.

--- src/tasks/dhtTask.cpp
+++ src/tasks/dhtTask.cpp
@@ -4,30 +4,56 @@
 #include <Arduino.h>
 
 extern QueueHandle_t dataQueue;
+extern EventGroupHandle_t networkEventGroup;
+#define NETWORK_CONNECTED_BIT BIT0
+
+void sendDHTData(sensor_data_t &dhtData)
+{
+    EventBits_t bits = xEventGroupGetBits(networkEventGroup);
+    if (bits & NETWORK_CONNECTED_BIT)
+    {
+        if (xQueueSend(dataQueue, &dhtData, portMAX_DELAY) != pdPASS)
+        {
+            Serial.println("[DHT Task] Failed to send data to queue");
+        }
+        else
+        {
+            Serial.println("[DHT Task] Data sent to queue successfully.");
+        }
+    }
+}
 
 void dhtTask(void *parameter)
 {
     SensorDHT dhtSensor(DHT_PIN);
-    sensor_data_t accData = {};
+    sensor_data_t dhtData = {};
+    float oldTemp = NAN;
+    float oldHum = NAN;
+    float newTemp = NAN;
+    float newHum = NAN;
 
     dhtSensor.begin();
 
     while (true)
     {
         dhtSensor.update();
-        accData.temperature = dhtSensor.getTemperature();
-        accData.humidity = dhtSensor.getHumdity();
+        newTemp = dhtSensor.getTemperature();
+        newHum = dhtSensor.getHumdity();
 
         Serial.print("[DHT Task] Temperature: ");
-        Serial.println(accData.temperature);
+        Serial.println(newTemp);
         Serial.print("[DHT Task] Humidity: ");
-        Serial.println(accData.humidity);
+        Serial.println(newHum);
 
-        if(xQueueSend(dataQueue, &accData, portMAX_DELAY) != pdPASS)
+        if (isnan(oldTemp) || isnan(oldHum) || fabs(newTemp - oldTemp) > TEMP_DELTA_THRESHOLD ||
+            fabs(newHum - oldHum) > HUM_DELTA_THRESHOLD)
         {
-            Serial.println("[DHT Task] Failed to send data to queue");
+            dhtData.temperature = newTemp;
+            dhtData.humidity = newHum;
+            sendDHTData(dhtData);
+            oldTemp = newTemp;
+            oldHum = newHum;
         }
-        
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
