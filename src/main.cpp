321f1b69db61aa92b3d2f280ce5598a4158a884e
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-26 00:11:52 +0200

    main: add semaphores and protect network event bit

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,10 @@ QueueHandle_t httpQueue;
 EventGroupHandle_t networkEventGroup;
 #define SYSTEM_READY_BIT BIT1
 
+SemaphoreHandle_t serialMutex;
+SemaphoreHandle_t modemMutex;
+SemaphoreHandle_t networkEventMutex;
+
 TinyGsm modem(SerialAT);
 
 void setup()
@@ -31,22 +35,38 @@ void setup()
     WiFi.mode(WIFI_STA);
     WiFi.disconnect(true);
 
+    serialMutex = xSemaphoreCreateMutex();
+    modemMutex = xSemaphoreCreateMutex();
+    networkEventMutex = xSemaphoreCreateMutex();
+
+    if (serialMutex == NULL || modemMutex == NULL || networkEventMutex == NULL) {
+        Serial.println("Failed to create mutexes!");
+        while(1);
+    }
+
     networkEventGroup = xEventGroupCreate();
 
-    dataQueue = xQueueCreate(10, sizeof(sensor_data_t));
-    httpQueue = xQueueCreate(10, sizeof(sensor_data_t));
+    dataQueue = xQueueCreate(10, sizeof(sensor_message_t));
+    httpQueue = xQueueCreate(10, sizeof(processed_data_t));
 
-    xTaskCreatePinnedToCore(accelTask, "AccelTask", 4096, NULL, 1, NULL, 1);
-    //xTaskCreate(bluetoothTask, "Bluetooth Task", 4096, NULL, 1, NULL);
-    xTaskCreate(dhtTask, "DHT Task", 2048, NULL, 1, NULL);
+    xTaskCreatePinnedToCore(accelTask, "AccelTask", 8192, NULL, 1, NULL, 1);
+    xTaskCreate(bluetoothTask, "Bluetooth Task", 4096, NULL, 1, NULL);
+    xTaskCreate(dhtTask, "DHT Task", 4096, NULL, 1, NULL);
     xTaskCreate(gasTask, "Gas Task", 4096, NULL, 1, NULL);
-    xTaskCreatePinnedToCore(communicationTask, "CommTask", 6144, NULL, 1, NULL,
-                            ARDUINO_RUNNING_CORE);
-    xTaskCreatePinnedToCore(networkStatusTask, "networkStatusTask", 2096, NULL, 1, NULL,
-                            ARDUINO_RUNNING_CORE);
+    xTaskCreatePinnedToCore(communicationTask, "CommTask", 8192, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(networkStatusTask, "networkStatusTask", 8192, NULL, 1, NULL, 1);
     xTaskCreate(processingTask, "Process", 4096, NULL, 1, NULL);
 
-    xEventGroupSetBits(networkEventGroup, SYSTEM_READY_BIT);
+    if (xSemaphoreTake(networkEventMutex, pdMS_TO_TICKS(1000)) == pdTRUE)
+    {
+        xEventGroupSetBits(networkEventGroup, SYSTEM_READY_BIT);
+        xSemaphoreGive(networkEventMutex);
+    }
+    else
+    {
+        Serial.println("Failed to initialize system ready bit!");
+        while(1);
+    }
 
     Serial.println("Sentinel started.");
 }
