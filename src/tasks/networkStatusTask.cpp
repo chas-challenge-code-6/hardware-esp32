04fcf02c2462a2997ec5fee6035d5cd43da3e8f9
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-25 15:48:48 +0200

    refactor: add SYSTEM_READY_BIT definition and ensure event group wait in networkStatusTask

--- src/tasks/networkStatusTask.cpp
+++ src/tasks/networkStatusTask.cpp
@@ -1,14 +1,18 @@
+#include "tasks/networkStatusTask.h"
 #include "network/network.h"
 #include "secrets.h"
 #include <Arduino.h>
 
 extern EventGroupHandle_t networkEventGroup;
 #define NETWORK_CONNECTED_BIT BIT0
+#define SYSTEM_READY_BIT BIT1
 
 Network network;
 
 void networkStatusTask(void *pvParameters)
 {
+    xEventGroupWaitBits(networkEventGroup, SYSTEM_READY_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+
     while (true)
     {
         network.maintainConnection(SSID, PASSWORD, NETWORK_APN);
