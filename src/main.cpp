0789fd3eaec00eae9da06dfa5fcf3f7c78174552
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-25 15:49:13 +0200

    refactor: reorganize setup function and define SYSTEM_READY_BIT for network event management

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@
 #include "tasks/communicationTask.h"
 #include "tasks/dhtTask.h"
 #include "tasks/gasTask.h"
+#include "tasks/networkStatusTask.h"
 #include "tasks/processingTask.h"
+#include "utilities.h"
+#include <TinyGsmClient.h>
+#include <WiFi.h>
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -15,24 +19,36 @@ QueueHandle_t dataQueue;
 QueueHandle_t httpQueue;
 
 EventGroupHandle_t networkEventGroup;
+#define SYSTEM_READY_BIT BIT1
+
+TinyGsm modem(SerialAT);
 
 void setup()
 {
     Serial.begin(115200);
     Wire.begin(SDA_PIN, SCL_PIN, 100000);
 
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect(true);
+
     networkEventGroup = xEventGroupCreate();
 
     dataQueue = xQueueCreate(10, sizeof(sensor_data_t));
     httpQueue = xQueueCreate(10, sizeof(sensor_data_t));
 
-    xTaskCreatePinnedToCore(accelTask, "AccelTask", 4096, NULL, 1, NULL,
-                            1); // Pin to core 1 to not disturb WiFi/LTE
-    xTaskCreate(bluetoothTask, "Bluetooth Task", 2048, NULL, 1, NULL);
-    // xTaskCreate(dhtTask, "DHT Task", 2048, NULL, 1, NULL);
-    // xTaskCreate(gasTask, "Gas Task", 2048, NULL, 1, NULL);
-    // xTaskCreate(communicationTask, "CommTask", 4096, &comm, 1, NULL);
-    // xTaskCreate(processingTask, "Process", 4096, NULL, 1, NULL);
+    xTaskCreatePinnedToCore(accelTask, "AccelTask", 4096, NULL, 1, NULL, 1);
+    //xTaskCreate(bluetoothTask, "Bluetooth Task", 4096, NULL, 1, NULL);
+    xTaskCreate(dhtTask, "DHT Task", 2048, NULL, 1, NULL);
+    xTaskCreate(gasTask, "Gas Task", 4096, NULL, 1, NULL);
+    xTaskCreatePinnedToCore(communicationTask, "CommTask", 6144, NULL, 1, NULL,
+                            ARDUINO_RUNNING_CORE);
+    xTaskCreatePinnedToCore(networkStatusTask, "networkStatusTask", 2096, NULL, 1, NULL,
+                            ARDUINO_RUNNING_CORE);
+    xTaskCreate(processingTask, "Process", 4096, NULL, 1, NULL);
+
+    xEventGroupSetBits(networkEventGroup, SYSTEM_READY_BIT);
+
+    Serial.println("Sentinel started.");
 }
 
 void loop() {}
