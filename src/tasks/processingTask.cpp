3cc4ea4ca3a893aebad73a551618ebc75430e019
Fredrik Storm <fredrik.storm@chasacademy.se>
2025-05-27 23:54:18 +0200

    processing: workaround some backend quirks.

--- src/tasks/processingTask.cpp
+++ src/tasks/processingTask.cpp
@@ -51,9 +51,10 @@ bool createJson(const sensor_data_t &data, char *buffer, size_t bufferSize)
                        "\"gas\": { \"ppm\": %d }, "
                        "\"fall_detected\": %d, "
                        "\"device_battery\": %d, "
+                       "\"watch_battery\": \"0\", "
                        "\"heart_rate\": %d, "
                        "\"noise_level\": %d } }",
-                       DEVICE_ID, data.steps, data.temperature, (int)data.humidity, data.gasLevel,
+                       DEVICE_ID, data.steps, data.temperature, (int)data.humidity, (int)data.gasLevel,
                        data.fall_detected, data.device_battery, data.heartRate, data.noise_level);
     if (len < 0 || len >= (int)bufferSize)
     {
